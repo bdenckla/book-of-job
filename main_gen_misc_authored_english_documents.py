633392729ca40f562a561e444405d621f8c58300
Ben Denckla <bdenckla@alum.mit.edu>
2026-01-09 14:04:18 -0500

    first somewhat-real commit

---
+++ main_gen_misc_authored_english_documents.py
@@ -0,0 +1,38 @@
+""" Exports main """
+
+from py import two_col_css_styles as tcstyles
+from py import my_html
+from pyauthor import prov1
+
+
+def write_index_dot_html(css_hrefs, out_path, prov1_anchor):
+    body_contents = (
+        prov1_anchor,
+    )
+    write_ctx = my_html.WriteCtx(
+        "Proverbs Documents", out_path, css_hrefs=css_hrefs
+    )
+    my_html.write_html_to_file(body_contents, write_ctx)
+
+
+def main():
+    # XXX TODO: rm *.html (to avoid stale files when output names change)
+    #
+    provn_rel_top = "docs/provn"
+    provn_rel_docs = "./provn"
+    #
+    css_href = "style.css"
+    tcstyles.make_css_file_for_authored(f"docs/{css_href}")
+    tcstyles.make_css_file_for_authored(f"{provn_rel_top}/{css_href}")
+    #
+    tdm_ch = provn_rel_top, css_href
+    #
+    prov1.gen_html_file(tdm_ch)
+    prov1_anchor = prov1.anchor(provn_rel_docs)
+    #
+    write_index_dot_html((css_href,), "docs/index.html", prov1_anchor)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ pyauthor/util/dollar_sub_g.py
@@ -0,0 +1,27 @@
+import re
+from py import my_html
+from pycmn.my_utils import sl_map
+
+
+def dollar_sub_g(dispatch, contents):
+    """The parameter "dispatch" is a dict.
+    It maps strings like "$tsinnorit" to functions that take no
+    arguments and return an HTML element (usually a span)."""
+    flat_1 = my_html.flatten(contents)
+    assert flat_1 is not None
+    return sl_map((_dollar_sub_flat_el, dispatch), flat_1)
+
+
+def _dollar_sub_flat_el(dispatch, flat_el):
+    if isinstance(flat_el, str):
+        return _dollar_sub_str(dispatch, flat_el)
+    return flat_el
+
+
+def _dollar_sub_str(dispatch, str):
+    parts = re.split("([$][a-zA-Z0-9_]+)", str)
+    return sl_map((_dollar_sub_str_part, dispatch), parts)
+
+
+def _dollar_sub_str_part(dispatch, part):
+    return dispatch[part] if part.startswith("$") else part
