b1a40c6577674d35716d31140d59589469fbaf43
Ben Denckla <bdenckla@alum.mit.edu>
2026-02-11 13:55:45 -0500

    22:28 intermediate

--- pyauthor_qr/qr_2228.py
+++ pyauthor_qr/qr_2228.py
@@ -20,6 +20,7 @@ RECORD_2228 = {
     "qr-lc-proposed": "א֖וֹמֶר",
     "qr-what-is-weird": "טרחא not דחי",
     "qr-consensus": "אֹ֭מֶר",
+    "qr-intermediate": "א֭וֹמֶר",
     "qr-generic-comment": [
         "The scribe of μL probably intended a דחי but placed it like a טרחא.",
         " The מלא/חסר spelling difference is not important to us here.",
