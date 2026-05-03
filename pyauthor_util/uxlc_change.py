0314375485021c529e0e6d56f4aa35c68e95466c
Ben Denckla <bdenckla@alum.mit.edu>
2026-05-03 05:47:58 -0400

    Use shared mb_cmn URL helpers for UXLC change links
    - add mb_cmn url_percent helper module
    - add mb_cmn uxlc_change_url helper module
    - refactor pyauthor UXLC change-link builder to delegate to shared helper
    - remove local URL assembly logic

--- pyauthor_util/uxlc_change.py
+++ pyauthor_util/uxlc_change.py
@@ -1,6 +1,5 @@
+from mb_cmn.uxlc_change_url import uxlc_change_url as mb_uxlc_change_url
+
+
 def uxlc_change(release, change_id):
-    return (
-        f"https://tanach.us/Changes/{release}%20-%20Changes/"
-        + f"{release}%20-%20Changes.xml?"
-        + f"{change_id}"
-    )
+    return mb_uxlc_change_url(release, change_id)
